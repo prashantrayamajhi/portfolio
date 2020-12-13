b82752e94b2de7117778e29d6fa065d9a6f86f25
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-12-13 15:58:42 +0545

    skills

--- src/components/skills/SkillsWrapper.jsx
+++ src/components/skills/SkillsWrapper.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './Skills.scss';
+
 export default function SkillsWrapper() {
     return (
-        <div id="skills">
-            
+        <div className="skills-wrapper" id="skills">
+            <h2>My Skills</h2>
+            <div className="skills-cards">
+                <div className="card">
+                    <h3>Frontend</h3>
+                    <ul>
+                        <li>HTML 5, CSS, JS</li>
+                        <li>SCSS</li>
+                        <li>Bootstrap</li>
+                        <li>J-Query</li>
+                        <li>React</li>
+                        <li>Typescript</li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <h3>Backend</h3>
+                    <ul>
+                        <li>Nodejs</li>
+                        <li>Typescript</li>
+                        <li>MySQL</li>
+                        <li>Postgres</li>
+                        <li>Mongodb</li>
+                    </ul>
+                </div>
+
+                <div className="card">
+                    <h3>Technologies</h3>
+                    <ul>
+                        <li>Github</li>
+                        <li>Visual Studio Code</li>
+                        <li>Codepen io</li>
+                        <li>Discord</li>
+                        <li>Stackoverflow</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
