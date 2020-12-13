1d6aaf6c5201749fc38e121d39c40a77077e0aef
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-12-13 14:23:50 +0545

    fixed navigation

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -12,10 +12,10 @@ export default function Navbar() {
 
             <ul className="nav-list">
                     <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#skills">Skills</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/#skills">Skills</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/#projects">Projects</a></li>
                     <li className="nav-item"><Link className="nav-link" to="/blog">Blog</Link></li>
-                    <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/#contact">Contact</a></li>
             </ul>
         </nav>
     )
