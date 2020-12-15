884ebc8aa5488d6478fb6c948d8dfaf414194c34
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-12-15 14:50:23 +0545

    blog page and responsive navbar

--- src/components/blog/BlogsWrapper.jsx
+++ src/components/blog/BlogsWrapper.jsx
@@ -3,8 +3,23 @@ import './Blog.scss';
 
 export default function BlogsWrapper() {
     return (
-        <div>
-            <h1>Blog</h1>
+        <div className="blog-wrapper">
+            <div className="blog">
+                <a href="/blog"><h2>Blog Title</h2></a>
+                <span>December 1, 2020</span>
+                <div className="tag">
+                    <a href="/blog">Frontend</a>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita commodi provident reiciendis tempore et odio deserunt, sint laborum temporibus, a, cupiditate molestiae animi delectus veritatis rem? Quam non adipisci nisi!</p>
+            </div>
+            <div className="blog">
+                <a href="/blog"><h2>Another Blog Title</h2></a>
+                <span>December 10, 2020</span>
+                <div className="tag">
+                    <a href="/blog">Backend</a>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita commodi provident reiciendis tempore et odio deserunt, sint laborum temporibus, a, cupiditate molestiae animi delectus veritatis rem? Quam non adipisci nisi!</p>
+            </div>
         </div>
     )
 }
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.scss'
 import {Link} from 'react-router-dom';
 
 
 export default function Navbar() {
+
+    // open or close menu
+    const [open, setOpen] = useState(false);
+
+    const onMenuClick = () => {
+        setOpen(!open)
+    }
+
     return (
         <nav className="navbar">
             <div className="logo">
                 <p><Link to="/" className="nav-brand">Prashant Rayamajhi</Link></p>
             </div>
 
-            <ul className="nav-list">
+            <ul className={`nav-list ${open ? 'responsive' : ''}`}>
                     <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
                     <li className="nav-item"><a className="nav-link" href="/#skills">Skills</a></li>
                     <li className="nav-item"><a className="nav-link" href="/#projects">Projects</a></li>
                     <li className="nav-item"><Link className="nav-link" to="/blog">Blog</Link></li>
                     <li className="nav-item"><a className="nav-link" href="/#contact">Contact</a></li>
             </ul>
+            <div className="hamburger">
+                <img onClick={onMenuClick} alt="" src="https://www.flaticon.com/svg/static/icons/svg/1636/1636053.svg" />
+            </div>
         </nav>
     )
 }
